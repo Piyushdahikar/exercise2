dec4be8ae882a7fca19e267dacec1c21999992b3
Piyushdahikar <39274085+Piyushdahikar@users.noreply.github.com>
2021-01-28 16:43:58 +0530

    exercise2
    Added code for exercise2

---
+++ once.js
@@ -0,0 +1,36 @@
+let functionMap = new Map();
+//fn --> function Name and count--> number of times functio to be called
+function callTImeOnly(fn, count){
+    return function(args){
+        let context = this;
+        if(functionMap.has(fn.name) && functionMap.get(fn.name) >= count){
+            return;
+        }else if(functionMap.has(fn.name) && functionMap.get(fn.name) < count){
+            let functionCount = functionMap.get(fn.name) + 1;
+            functionMap.set(fn.name, functionCount);
+            return fn.apply(this, args);
+        }else{
+            functionMap.set(fn.name, 1);
+            return fn.apply(this, args);
+        }
+    }
+}
+
+// Example 1---------> called twice
+let count =0;
+function counter(){
+    console.log(countE);
+    return countE++;
+}
+callTImeOnly(counter, 2)();
+callTImeOnly(counter, 2)();
+callTImeOnly(counter, 2)();
+callTImeOnly(counter, 2)();
+callTImeOnly(counter, 2)();
+
+// Example 2 ----> called once
+function display(){
+    console.log("display called");
+}
+callTImeOnly(display, 1)();
+callTImeOnly(display, 1)();
